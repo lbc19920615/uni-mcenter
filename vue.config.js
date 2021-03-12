74f4d0ded5f92d3e5273951be422b224ac67426f
lbc19920615 <1585638808@qq.com>
2021-03-12 20:03:08 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
 module.exports = {
- css: {
-	loaderOptions: {
-	  sass: {
-		implementation: require('sass')
-	  }
-	}
+ 	css: {
+		loaderOptions: {
+			sass: {
+				implementation: require('sass')
+			}
+		}
   }
 }
