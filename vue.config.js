ee8018d79f7b35c23c672663530b91b29c095940
lbc19920615 <1585638808@qq.com>
2021-03-23 14:25:16 +0800

    add vue cli plugin

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,24 @@
+const TransformPages = require('uni-read-pages')
+const {webpack} = new TransformPages()
+
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
+	configureWebpack: {
+		plugins: [
+			new webpack.DefinePlugin({
+				PRO_FOO: webpack.DefinePlugin.runtimeValue(() => {
+					return JSON.stringify({})
+				}, true )
+			})
+		]
+	},
  	css: {
 		loaderOptions: {
 			sass: {
 				implementation: require('sass')
 			}
 		}
-  }
+  },
 }
