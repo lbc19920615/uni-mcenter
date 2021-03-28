1b695e57cd97158214f41d3e6fee02770e1b536a
凌柏超 <1585638808@qq.com>
2021-03-28 11:42:16 +0800

    Update var.js

--- src/var.js
+++ src/var.js
@@ -1,25 +1,23 @@
-
-
 export let demoPages = [
 	{
 		name: "图片拖动排序",
 		icon: "/static/c1.png",
-		url: "/pages/demos/test",
+		url: "/pages/demos/test"
 	},
 	{
 		name: "类似于element表单",
 		icon: "/static/c1.png",
-		url: "/pages/demos/form",
+		url: "/pages/demos/form"
 	},
 	{
 		name: "布局",
 		icon: "/static/c1.png",
-		url: "/pages/demos/layout",
+		url: "/pages/demos/layout"
 	},
 	{
 		name: "左右滚动带指示栏",
 		icon: "/static/c1.png",
-		url: "/pages/demos/scroll",
+		url: "/pages/demos/scroll"
 	},
 	// {
 	// 	name: "新首页",
@@ -29,44 +27,44 @@ export let demoPages = [
 	{
 		name: "常见UI",
 		icon: "/static/c1.png",
-		url: "/sub_pages/demos/commonui",
+		url: "/sub_pages/demos/commonui"
 	},
 	{
 		name: "下拉菜单",
 		icon: "/static/c1.png",
-		url: "/pages/demos/dropdown",
+		url: "/pages/demos/dropdown"
 	},
 	{
 		name: "cavans和海报",
 		icon: "/static/c1.png",
-		url: "/pages/demos/canvas",
+		url: "/pages/demos/canvas"
 	},
 	{
 		name: "瑞幸 购买列表",
 		icon: "/static/c1.png",
-		url: "/pages/demos/rx_list",
+		url: "/pages/demos/rx_list"
 	},
 	{
-		name: "native socket",
+		name: "socket.io",
 		icon: "/static/c1.png",
-		url: "/sub_pages/demos/native-socket",
+		url: "/sub_pages/demos/native-socket"
 	},
 	{
 		name: "弹窗login",
 		icon: "/static/c1.png",
-		url: "/pages/auth/login",
+		url: "/pages/auth/login"
 	},
 	{
 		name: "搜索页面",
 		icon: "/static/c1.png",
-		url: "/pages/demos/search_page",
-	},
-]
+		url: "/pages/demos/search_page"
+	}
+];
 
 export let miniPages = [
 	{
 		name: "相机",
 		icon: "/static/c1.png",
-		url: "/pages/demos/mini_camera",
-	},
-]
+		url: "/pages/demos/mini_camera"
+	}
+];
