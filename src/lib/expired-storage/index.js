c15098a14601e1323aeeece0e520b0bf3f0c60f2
凌柏超 <1585638808@qq.com>
2021-03-28 17:46:43 +0800

    Update index.js

--- src/lib/expired-storage/index.js
+++ src/lib/expired-storage/index.js
@@ -22,7 +22,7 @@ class Expire {
 	 * @param amount 数量
 	 * @param unit 单位 @see EXPIRE_UNIT
 	 * */
-	constructor (amount, unit) {
+	constructor(amount, unit) {
 		let offset = Math.max(1, parseFloat(amount));
 		switch (unit || EXPIRE_UNIT.SECOND) {
 			case EXPIRE_UNIT.MINUTE:
@@ -42,24 +42,28 @@ class Expire {
 	}
 }
 
+/**
+ * Storage
+ */
 class Storage {
-	constructor () {
+	constructor() {
 		this.EXPIRE_UNIT = EXPIRE_UNIT;
 		this.Expire = Expire;
 		// const supportWechatMiniProgram = typeof Wechat.getStorageSync === 'function' && typeof Wechat.setStorageSync === 'function' && typeof Wechat.removeStorageSync === 'function';
-		this.set = function (key, data, expire) {
+		this.set = function(key, data, expire) {
 			expire = expire || new this.Expire(30, this.EXPIRE_UNIT.DAY);
 			const value = JSON.stringify({
-				data, expire: expire.expireDate
+				data,
+				expire: expire.expireDate
 			});
-			return localStorage['setItem'](key, value);
+			return localStorage["setItem"](key, value);
 			// return supportWechatMiniProgram ? Wechat['setStorageSync'](key, value) : LocalStorage['setItem'](key, value);
 		};
 
-		this.get = function (key) {
+		this.get = function(key) {
 			// const value = supportWechatMiniProgram ? Wechat['getStorageSync'](key) : LocalStorage['getItem'](key);
-			const value = localStorage['getItem'](key);
-			const _package = JSON.parse(value || '{}');
+			const value = localStorage["getItem"](key);
+			const _package = JSON.parse(value || "{}");
 			if (Date.now() < _package.expire) {
 				return _package.data;
 			}
@@ -67,8 +71,8 @@ class Storage {
 			return null;
 		};
 
-		this.remove = function (key) {
-			return localStorage['removeItem'](key);
+		this.remove = function(key) {
+			return localStorage["removeItem"](key);
 			// return supportWechatMiniProgram ? Wechat['removeStorageSync'](key) : LocalStorage['removeItem'](key);
 		};
 	}
