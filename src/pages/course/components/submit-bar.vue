2639790a28fca6f0faff98d4bcc1305c99b4ce6f
lbc19920615 <1585638808@qq.com>
2021-03-29 11:19:54 +0800

    提交按钮

--- src/pages/course/components/submit-bar.vue
+++ src/pages/course/components/submit-bar.vue
@@ -1,12 +1,30 @@
 <template>
   <view class="submit-bar">
-    <div class="submit-bar-inner">submit-bar-inner</div>
+    <view class="ui-flex ui-items-center submit-bar-inner">
+		<view class="submit-bar__content">
+			<text style="margin-left: 10rpx;">11212</text>
+		</view>
+		<view class="ui-flex ui-items-center ui-justify-center submit-bar__action">{{getButtonText}}</view>
+	</view>
   </view>
 </template>
 
 <script>
 export default {
-  name: "submit-bar"
+  name: "submit-bar",
+  data() {
+	  return {
+		  getButtonText: ''
+	  }
+  },
+  created() {
+	  this.render()
+  },
+  methods: {
+	  render() {
+		  this.getButtonText = '立即购买'
+	  }
+  }
 }
 </script>
 
@@ -20,6 +38,16 @@ export default {
   padding-bottom: var(--safe-area-inset-bottom);
   &-inner {
     height: 100%;
+  }
+  &__content {
+	  flex: 1;
+	  box-sizing: border-box;
+  }
+  &__action {
+	  width: 240rpx;
+	  height: 100%;
+	  line-height: 1;
+	  background-color: #6993ff;
   }
 }
 </style>
