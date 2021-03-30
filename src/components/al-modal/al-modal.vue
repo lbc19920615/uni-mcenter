37a9a64ac83192218e143d675a4460c2bb53dbeb
lbc19920615 <1585638808@qq.com>
2021-03-30 13:23:17 +0800

    add modal

---
+++ src/components/al-modal/al-modal.vue
@@ -0,0 +1,67 @@
+<style lang="scss">
+@import "../../styles/alh";
+
+$tag: "al-modal";
+$sel: ".al-modal";
+
+#{$sel} {
+  $headerHeight: 60rpx;
+  --al-login-modal-bg-color: var(--background-color-common);
+  //background-color: var(--al-login-modal-bg-color, --background-color-common);
+  background-color: var(--al-login-modal-bg-color);
+  &__header {
+    height: $headerHeight;
+  }
+  &__title {
+    flex: 1;
+  }
+  &__close {
+    @include size(60rpx);
+    font-size: 36rpx;
+  }
+}
+</style>
+
+<template>
+<uni-popup :type="position" ref="pop">
+  <view class="al-modal">
+    <view class="ui-flex ui-items-center al-modal__header">
+      <view class="al-modal__title"><text :decode="true">&nbsp;</text></view>
+      <view class="ui-flex ui-items-center ui-justify-center al-modal__close"
+            @click="handleClose"
+      >x</view>
+    </view>
+    <slot></slot>
+  </view>
+</uni-popup>
+</template>
+
+<script>
+import UniPopup from "@/uni_modules/uni-popup/components/uni-popup/uni-popup";
+export default {
+  name: "al-modal",
+  mixins: [],
+  components: {UniPopup},
+  props: {
+    position: String
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    open() {
+      this.visibile = true
+      this.$refs.pop.open()
+    },
+    close() {
+      this.$refs.pop.close()
+    },
+    onClosed() {
+      this.visibile = false
+    },
+    handleClose() {
+      this.close()
+    }
+  }
+}
+</script>
--- src/sub_pages/demos/modal.vue
+++ src/sub_pages/demos/modal.vue
@@ -9,19 +9,31 @@ $sel: ".modal";
 
 <template>
 <app-page class="page-modal">
-  modal
+  <button @click="openModal('modal1')">open center modal</button>
+  <button @click="openModal('modal2')">open bottom modal</button>
+  <al-modal position="center" ref="modal1">
+    <view>texttexttext</view>
+  </al-modal>
+  <al-modal position="bottom" ref="modal2">
+    <view>texttexttext</view>
+  </al-modal>
 </app-page>
 </template>
 
 <script>
+import AlModal from "@/components/al-modal/al-modal";
 export default {
   name: "modal",
   mixins: [],
-  components: {},
+  components: {AlModal},
   props: {},
   data() {
     return {}
   },
-  methods: {}
+  methods: {
+    openModal(name) {
+      this.$refs[name].open()
+    }
+  }
 }
 </script>
