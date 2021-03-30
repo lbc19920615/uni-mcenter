9b9c40c9513d6c55c98f6dcab185ed38e9d1f9dd
lbc19920615 <1585638808@qq.com>
2021-03-30 11:50:54 +0800

    Update actionsheet.vue

--- src/sub_pages/demos/actionsheet.vue
+++ src/sub_pages/demos/actionsheet.vue
@@ -9,19 +9,68 @@ $sel: ".actionsheet";
 
 <template>
 <app-page class="page-actionsheet">
-  actionsheet
+  <button @click="openPicker1">打开picker1</button>
+  <button @click="openPicker2">打开picker2</button>
+  <lb-picker ref="picker1"
+             v-model="value1"
+             :list="list1"></lb-picker>
+  <lb-picker ref="picker2"
+             v-model="value2"
+             mode="unlinkedSelector"
+             :list="list2"></lb-picker>
 </app-page>
 </template>
 
 <script>
+import LbPicker from 'uni-lb-picker'
+
 export default {
 name: "actionsheet",
 mixins: [],
-components: {},
+components: {
+  LbPicker
+},
 props: {},
 data() {
-    return {}
+    return {
+      value1: '',
+      list1: [
+        {
+          label: '选项1',
+          value: '1'
+        },
+        {
+          label: '选项2',
+          value: '2'
+        }
+      ],
+      value2: [],
+      list2: [
+        [
+          { label: '选项1', value: '1' },
+          { label: '选项2', value: '2' },
+          { label: '选项3', value: '3' }
+        ],
+        [
+          { label: '选项11', value: '11' },
+          { label: '选项22', value: '22' },
+          { label: '选项33', value: '33' }
+        ],
+        [
+          { label: '选项111', value: '111' },
+          { label: '选项222', value: '222' },
+          { label: '选项333', value: '333' }
+        ]
+      ]
+    }
 },
-methods: {}
+methods: {
+  openPicker1() {
+    this.$refs.picker1.show()
+  },
+  openPicker2() {
+    this.$refs.picker2.show()
+  }
+}
 }
 </script>
