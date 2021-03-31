7cb9519745be824c0df7e17984b9b54a5393a40a
lbc19920615 <1585638808@qq.com>
2021-03-31 09:54:28 +0800

    add checkout page

---
+++ src/sub_pages/demos/components/pay-type-select.vue
@@ -0,0 +1,27 @@
+<style lang="scss">
+$tag: "pay-type-select";
+$sel: ".pay-type-select";
+
+#{$sel} {
+
+}
+</style>
+
+<template>
+  <view class="pay-type-select">
+    pay-type-select
+  </view>
+</template>
+
+<script>
+export default {
+  name: "pay-type-select",
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
---
+++ src/sub_pages/demos/demos-checkout.vue
@@ -0,0 +1,28 @@
+<style lang="scss">
+$tag: "page-demos-checkout";
+$sel: ".page-demos-checkout";
+
+#{$sel} {
+  font-size: 32rpx;
+}
+</style>
+
+<template>
+<app-page class="page-demos-checkout">
+  <pay-type-select></pay-type-select>
+</app-page>
+</template>
+
+<script>
+import PayTypeSelect from "@/sub_pages/demos/components/pay-type-select";
+export default {
+name: "demos-checkout",
+mixins: [],
+components: {PayTypeSelect},
+props: {},
+data() {
+    return {}
+},
+methods: {}
+}
+</script>
