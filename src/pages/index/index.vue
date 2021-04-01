54c20228d9e87bf2349d4f8cd239667a068b1981
凌柏超 <1585638808@qq.com>
2021-04-01 22:27:09 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,22 +1,78 @@
 <template>
-	<app-page>
-<!--    <index-header-->
-<!--                  placeholder="考研政治徐涛"-->
-<!--    @click-action="clickAction"-->
-<!--    ></index-header>-->
-		<view class="swiper"
-          :autoplay="true">
-			<swiper :current="swiperIndex"
-              class="swiper-1"
-              :interval="1000"
-              >
-				<swiper-item v-for="(item, index) in list"
+  <app-page page-class="page-index-index">
+        <index-header
+            placeholder="考研政治徐涛"
+            @click-action="clickAction"
+        ></index-header>
+    <scroll-view class="nav-scroll"
+                 :enable-flex="true"
+                 scroll-with-animation
+                 :throttle="false"
+                 :scroll-left="scrollToLeft" scroll-x @scroll="handleScroll">
+      <view class="nav uni-nav">
+        <view class="nav-item"
+              :class="swiperIndex == index ? 'nav-item-act' : ''"
+              :key="index" v-for="(item, index) in list" @click="taggleNav(index)">
+          {{ item.title }}
+        </view>
+        <view class="nav-line" :style="style"></view>
+      </view>
+    </scroll-view>
+    <view class="swiper">
+      <swiper :current="swiperIndex"
+              :duration="300" class="swiper-1"
+              easing-function="linear"
+              @change="swiperChange"
+      >
+        <swiper-item v-for="(item, index) in list"
                      class="index-fragment"
+                     :class="['index-fragment-' + index]"
                      :key="item.uuid">
-          <view class="other-griditem" > <text class="text"> ali {{index}}</text></view>
-				</swiper-item>
-			</swiper>
-		</view>
+          <scroll-view
+              :lower-threshold="80"
+              :refresher-triggered="refreStatus"
+              @refresherrefresh="handleRefresh"
+              :refresher-threshold="50"
+              :upper-threshold="30"
+              :refresher-enabled="true"
+              class="swiper-scroll"
+              scroll-y="true"
+              @scroll="swiperScroll"
+              @scrolltoupper="swiperScrolltoupper"
+              @scrolltolower="swiperScrollLower"
+          >
+            <view class="swiper-item-wrap">
+              <view class="swiper-item-list"
+                    v-for="(sub_item, sub_item_index) in item.content"
+
+                    :key="sub_item_index">
+                <template v-if="index === 0">
+                  <!--      start 首页            -->
+                  <view class="ui-m-t-20 ui-m-b-20"
+                        v-if="sub_item_index === 0"
+                  >
+                    <index-swiper></index-swiper>
+                  </view>
+                  <view class="other-griditem"  v-else-if="sub_item_index === 1" >
+                    <index-routes></index-routes>
+                  </view>
+                  <view class="other-griditem" v-else>
+                    <app-class-card :item="sub_item"
+                                    :index="sub_item_index"
+                                    @click-card="onClickCard"
+                    ></app-class-card>
+                  </view>
+                  <!--      end 首页            -->
+                </template>
+                <template v-else>
+                  <view class="other-griditem" > <text class="text"> {{sub_item_index}}</text> <text class="text">{{sub_item}}</text></view>
+                </template>
+              </view>
+            </view>
+          </scroll-view>
+        </swiper-item>
+      </swiper>
+    </view>
   </app-page>
 </template>
 
@@ -30,180 +86,296 @@ import AppClassCard from "@/components/app-class-card/app-class-card";
 import IndexHeader from "@/pages/index/components/index-header";
 
 const mockData = [
-				{ title: '首页', content: ['首页-1', '首页-2', '首页-3', '首页-4',  '首页-5', '首页-6', '首页-7'] },
-				{ title: '测试', content: ['测试-1', '测试-2', '测试-3', '测试-4', '测试-5'] },
-				{ title: '我的', content: ['我的-1', '我的-2', '我的-3',] },
-				{ title: 'hello', content: ['hello-1', 'hello-2', 'hello-3', 'hello-4', 'hello-5'] },
-				{ title: '测试-1', content: ['测试-1-1', '测试-1-2', '测试-1-3', '测试-1-4', '测试-1-5'] },
-				{ title: '测试-2', content: ['测试-2-1', '测试-2-2', '测试-2-3', '测试-2-4', '测试-2-5'] },
-				{ title: '测试-3', content: ['测试-3-1', '测试-3-2', '测试-3-3', '测试-3-4', '测试-3-5'] },
-				{ title: '测试-4', content: ['测试-4-1', '测试-4-2', '测试-4-3', '测试-4-4', '测试-4-5'] },
-				{ title: '测试-5', content: ['测试-5-1', '测试-5-2', '测试-5-3', '测试-5-4', '测试-5-5'] }
-			]
+  { title: '首页', content: ['首页-1', '首页-2', '首页-3', '首页-4',  '首页-5', '首页-6', '首页-7'] },
+  { title: '测试', content: ['测试-1', '测试-2', '测试-3', '测试-4', '测试-5'] },
+  { title: '我的', content: ['我的-1', '我的-2', '我的-3',] },
+  { title: 'hello', content: ['hello-1', 'hello-2', 'hello-3', 'hello-4', 'hello-5'] },
+  { title: '测试-1', content: ['测试-1-1', '测试-1-2', '测试-1-3', '测试-1-4', '测试-1-5'] },
+  { title: '测试-2', content: ['测试-2-1', '测试-2-2', '测试-2-3', '测试-2-4', '测试-2-5'] },
+  { title: '测试-3', content: ['测试-3-1', '测试-3-2', '测试-3-3', '测试-3-4', '测试-3-5'] },
+  { title: '测试-4', content: ['测试-4-1', '测试-4-2', '测试-4-3', '测试-4-4', '测试-4-5'] },
+  { title: '测试-5', content: ['测试-5-1', '测试-5-2', '测试-5-3', '测试-5-4', '测试-5-5'] }
+]
+
+let indexHeaderMixin = {
+  methods: {
+    clickAction(type) {
+      if (type === 'action0') {
+        router.push({
+          path: '/pages/demos/search_page'
+        })
+      }
+    }
+  }
+}
+
+let indexCardMixin = {
+  methods: {
+    onClickCard(e) {
+      router.push({
+        name: 'course_detail',
+        params: {
+          course_uuid: Date.now()
+        }
+      })
+    }
+  }
+}
 
 export default {
   components: {IndexHeader, AppClassCard, IndexRoutes, IndexSwiper},
   mixins: [
-
-	],
-	data() {
-		return {
-			swiperIndex: 0, //当前的swiperindex
-			navItemWidth: '', //选中下划线的宽度
-			navItemLeft: 0, //选中下划线的显示位置
-			scrollToLeft: 0, //scrollview需要滚动的距离
-			navInfos: [], //所有navitem的节点信息
-			parentLeft: 0, //nav盒子的节点信息
-			componentWidth: 0, //nav盒子的宽度
-			// 防止在中途就刷新 确保到顶部下拉
-			IsSwiperScrolltoupper: true,
-			list: mockData.map(v => {
-				v.uuid = shortUUID.generate()
-				return v
-			}),
-			demoList: demoPages,
-			refreStatus: false
-		};
-	},
-	computed: {
-		style() {
-			return `width:${this.navItemWidth}px; left:${this.navItemLeft}px`;
-		}
-	},
+    indexHeaderMixin,
+    indexCardMixin,
+  ],
+  data() {
+    return {
+      swiperIndex: 0, //当前的swiperindex
+      navItemWidth: '', //选中下划线的宽度
+      navItemLeft: 0, //选中下划线的显示位置
+      scrollToLeft: 0, //scrollview需要滚动的距离
+      navInfos: [], //所有navitem的节点信息
+      parentLeft: 0, //nav盒子的节点信息
+      componentWidth: 0, //nav盒子的宽度
+      // 防止在中途就刷新 确保到顶部下拉
+      IsSwiperScrolltoupper: true,
+      list: mockData.map(v => {
+        v.uuid = shortUUID.generate()
+        return v
+      }),
+      demoList: demoPages,
+      refreStatus: false
+    };
+  },
+  computed: {
+    style() {
+      return `width:${this.navItemWidth}px; left:${this.navItemLeft}px`;
+    }
+  },
   onLoad() {
-		this.$nextTick(() => {
+    this.$nextTick(() => {
       // console.log('首页onLoad');
-	// #ifdef MP-WEIXIN || H5
-	 this.init();
-	// #endif
+      // #ifdef MP-WEIXIN || H5
+      this.init();
+      // #endif
+      //  #ifdef MP-ALIPAY
+      setTimeout(() => {
+        this.init();
+      }, 1000)
+      // #endif
     })
-	},
-	methods: {
-		// 获取dom信息
-		init() {
-			const query = uni.createSelectorQuery().in(this);
-			query.select('.uni-nav').fields({ rect: true, size: true }, res => {
-				this.parentLeft = res.left;
-				this.componentWidth = res.width;
-				console.log('res==>', res);
-			});
-			query.selectAll('.nav-item').fields({ rect: true, size: true }, data => {
-				data.forEach((item, index) => {
-					if (index == 0) {
-						this.navItemWidth = item.width;
-						this.navItemLeft = item.left;
-					}
-					this.navInfos.push({ width: item.width, left: item.left });
-				});
-			});
-			query.exec();
-		},
-		// 点击导航切换swiper
-		taggleNav(val) {
-			this.swiperIndex = val;
-		},
-		// 滚动tabs以及移动下划线
-		scrollDom() {
-			let info = this.navInfos[this.swiperIndex];
-			let offsetLeft = info.left - this.parentLeft;
-			let scrollLeft = offsetLeft - (this.componentWidth - info.width) / 2;
-			this.scrollToLeft = scrollLeft < 0 ? 0 : scrollLeft;
-			this.navItemLeft = this.navInfos[this.swiperIndex].left;
-			setTimeout(() => {
-				this.navItemWidth = info.width;
-			}, 50);
-		},
-		// swiper的index变动
-		swiperChange(e) {
-			this.swiperIndex = e.detail.current;
-			this.scrollDom();
-			this.$emit('currentIndex', this.swiperIndex);
-		},
-		// tabs-scrollview触底
-		handleScroll(e) {
-			this.scrollDom();
-		},
-		swiperScroll(e) {
-			const {scrollLeft, scrollTop, scrollHeight, scrollWidth, deltaX, deltaY} = e.detail
-			// console.log('scrollTop', scrollTop)
-			if (scrollTop > 80) {
-				this.IsSwiperScrolltoupper = false
-			} else {
-				this.IsSwiperScrolltoupper = true
-			}
-		},
-		swiperScrolltoupper() {
-			console.log('swiperScrolltoupper')
-			// this.IsSwiperScrolltoupper = true
-		},
-		// swiper-ScrollLower触底
-		swiperScrollLower() {
-			uni.showToast({
-				icon: 'none',
-				title: `此时为${this.list[this.swiperIndex].title}触底`
-			});
-			setTimeout(() => {
-				this.getData();
-			}, 500);
-		},
-		// 生成列表数据
-		getData() {
-			uni.showLoading({
-				title: '加载中'
-			});
-			setTimeout(() => {
-				for (let index = 0; index < 10; index++) {
-					let arr = this.list[this.swiperIndex].content;
-					this.$set(arr, arr.length, Math.random() + '-' + index + this.list[this.swiperIndex].title);
-				}
-				uni.hideLoading();
-			}, 1000);
-			console.log(this.list[this.swiperIndex]);
-		},
-		// 下拉事件
-		handleRefresh() {
-			if (this.IsSwiperScrolltoupper) {
-				this.refreStatus = true;
-				uni.showLoading({
-					title: '下拉加载中'
-				});
-				setTimeout(() => {
-					this.list[this.swiperIndex].content = [];
-					for (var i = 0; i < 5; i++) {
-						this.list[this.swiperIndex].content.push(
+  },
+  methods: {
+    // 获取dom信息
+    init() {
+      // const query = uni.createSelectorQuery().in(this);
+      const query = uni.createSelectorQuery();
+      query.select('.uni-nav').fields({ rect: true, size: true }, res => {
+        this.parentLeft = res.left;
+        this.componentWidth = res.width;
+        console.log('res==>', res);
+      });
+      query.selectAll('.nav-item').fields({ rect: true, size: true }, data => {
+        data.forEach((item, index) => {
+          if (index == 0) {
+            this.navItemWidth = item.width;
+            this.navItemLeft = item.left;
+          }
+          this.navInfos.push({ width: item.width, left: item.left });
+        });
+      });
+      query.exec();
+    },
+    // 点击导航切换swiper
+    taggleNav(val) {
+      this.swiperIndex = val;
+      // #ifdef MP-ALIPAY
+      console.log('this.swiperIndex', this.swiperIndex)
+      this.scrollDom();
+      // #endif
+    },
+    // 滚动tabs以及移动下划线
+    scrollDom() {
+      let info = this.navInfos[this.swiperIndex];
+      let offsetLeft = info.left - this.parentLeft;
+      let scrollLeft = offsetLeft - (this.componentWidth - info.width) / 2;
+      this.scrollToLeft = scrollLeft < 0 ? 0 : scrollLeft;
+      this.navItemLeft = this.navInfos[this.swiperIndex].left;
+      setTimeout(() => {
+        this.navItemWidth = info.width;
+      }, 50);
+    },
+    // swiper的index变动
+    swiperChange(e) {
+      this.swiperIndex = e.detail.current;
+      this.scrollDom();
+      this.$emit('currentIndex', this.swiperIndex);
+    },
+    // tabs-scrollview触底
+    handleScroll(e) {
+      this.scrollDom();
+    },
+    swiperScroll(e) {
+      const {scrollLeft, scrollTop, scrollHeight, scrollWidth, deltaX, deltaY} = e.detail
+      // console.log('scrollTop', scrollTop)
+      if (scrollTop > 80) {
+        this.IsSwiperScrolltoupper = false
+      } else {
+        this.IsSwiperScrolltoupper = true
+      }
+    },
+    swiperScrolltoupper() {
+      console.log('swiperScrolltoupper')
+      // this.IsSwiperScrolltoupper = true
+    },
+    // swiper-ScrollLower触底
+    swiperScrollLower() {
+      uni.showToast({
+        icon: 'none',
+        title: `此时为${this.list[this.swiperIndex].title}触底`
+      });
+      setTimeout(() => {
+        this.getData();
+      }, 500);
+    },
+    // 生成列表数据
+    getData() {
+      uni.showLoading({
+        title: '加载中'
+      });
+      setTimeout(() => {
+        for (let index = 0; index < 10; index++) {
+          let arr = this.list[this.swiperIndex].content;
+          this.$set(arr, arr.length, Math.random() + '-' + index + this.list[this.swiperIndex].title);
+        }
+        uni.hideLoading();
+      }, 1000);
+      console.log(this.list[this.swiperIndex]);
+    },
+    // 下拉事件
+    handleRefresh() {
+      if (this.IsSwiperScrolltoupper) {
+        this.refreStatus = true;
+        uni.showLoading({
+          title: '下拉加载中'
+        });
+        setTimeout(() => {
+          this.list[this.swiperIndex].content = [];
+          for (var i = 0; i < 5; i++) {
+            this.list[this.swiperIndex].content.push(
                 this.list[this.swiperIndex].title + '下拉-' + i
             );
-					}
-					uni.hideLoading();
-				}, 1000);
-				setTimeout(() => {
-					this.refreStatus = false;
-				}, 1000);
-			} else {
-				setTimeout(() => {
-				uni.hideLoading();
-				}, 0)
-			}
-		}
-	}
+          }
+          uni.hideLoading();
+        }, 1000);
+        setTimeout(() => {
+          this.refreStatus = false;
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          uni.hideLoading();
+        }, 0)
+      }
+    }
+  }
 };
 </script>
 
 <style lang="scss" >
 @import "../../styles/alh";
 
+$headerBarHeight: 80rpx;
+
+.app-page {
+  padding-top: $headerBarHeight;
+}
+
+@include def-com-style('.index-header') {
+  height: $headerBarHeight;
+}
+
 page, .app-page {
-	height: 100%;
+  height: 100%;
+}
+
+.container {
+  height: 100%;
+  overflow: hidden;
+}
+
+$navHeight: 60rpx;
+$paddingV: 20rpx;
+
+.nav-item {
+  display: inline-block;
+  margin: 0 16rpx;
+  text-align: center;
+  transition: color 0.3s ease;
+}
+
+.nav {
+  white-space: nowrap;
+  position: relative;
+  height: $navHeight;
+  padding: 20rpx 0;
+  font-size: 32rpx;
+}
+
+.nav-item-act {
+  color: pink;
+  font-weight: bolder;
+}
+
+.nav-line {
+  position: absolute;
+  bottom: 0;
+  height: 10rpx;
+  border-radius: 10rpx;
+  background-color: pink;
+  transition: left 0.3s ease;
 }
 
 .swiper {
-  height: 100vh;
-  > .swiper-1 {
+  height: calc(100% - #{$navHeight + $paddingV * 2});
+  > swiper {
     height: 100% !important;
   }
-  .index-fragment {
-    height: 100%;
+}
+
+$swiperListItemHeight: 600rpx;
+.index-fragment {
+  height: 100%;
+
+  .other-griditem {
+    padding: 0 30rpx;
+    .text {
+      font-size: 32rpx;
+    }
+  }
+
+  &:not(.index-fragment-0) {
+    .other-griditem {
+      height: $swiperListItemHeight;
+    }
   }
 }
+
+.index-fragment-0 {
+  .swiper-item-wrap {
+
+    background-image: linear-gradient(
+            180deg
+        , rgba(245, 245, 250, 0) 350rpx, 750rpx, #F5F5FA);
+  }
+
+  .other-griditem {
+    margin-top: 30rpx;
+  }
+}
+
+.swiper-scroll {
+  height: 100%;
+}
+
 
 </style>
